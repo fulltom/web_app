1b88253349e4559892d7de73172ac9afcc8f51e3
fulltom <vergnaudt@gmail.com>
2015-02-24 18:32:22 +0100

    WOP form

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,18 +1,15 @@
 class StaticController < ApplicationController
-
+	layout :false
 	def index
 		@message = Message.new
 	end
 	def create
 		@message = Message.new(message_params)
-		if @message.valid?
-			flash[:error] = "Somethig is wrong"
-			redirect_to root_url
-		else
-			flash[:notice] = "Some text indicating it was created"
-			@message.save
-			redirect_to root_url
-		end
+		 if @message.save
+		    render 'index'
+		  else
+		     render 'index'
+		  end
 	end
 	private
 		def message_params
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
-	validates_presence_of(:name, :email, :body)
+
+
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :body, presence: true
 	# attr_accessor :message, :email, :body
 	# validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 	# validates_length_of :body, :maximum => 200
